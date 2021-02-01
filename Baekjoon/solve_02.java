a5221fc8b44ada97611ed0fa31a6734ca3f0a10f
Hyun-Jun Ha <76872285+s-rachmaninoff@users.noreply.github.com>
2021-02-01 16:43:10 +0900

    [Solve] 고양이
    백준 10171 고양이

---
+++ Baekjoon/solve_02.java
@@ -0,0 +1,10 @@
+// 10171 고양이
+
+public class Main{
+    public static void main(String[] args ){
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( \')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
