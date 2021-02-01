e368127bde48180291677905687bd2da12d70cd8
s-rachmaninoff <campanella07@naver.com>
2021-02-01 16:37:46 +0900

    [Solve] 제리와 톰

---
+++ Baekjoon/solve_01.java
@@ -0,0 +1,15 @@
+// 제리와 톰
+
+import java.util.*;
+
+public class Main {
+	public static void main(String []args) {
+		Scanner sc= new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		
+		System.out.println(b-a+" "+b);
+	}
+}
+
+ 
